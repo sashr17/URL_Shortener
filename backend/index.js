7e841850a03425a89d9f8a20d9dac3e2b445e890
Santhosh R <sashr17@gmail.com>
2021-03-08 14:54:11 +0530

    Moved server side changes to backend folder

--- backend/index.js
+++ backend/index.js
@@ -14,4 +14,4 @@ app.use("/api/url", require("./routes/url"));
 
 const PORT = 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`**** Server running on port ${PORT} ****`));
